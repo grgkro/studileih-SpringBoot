60819a17ff05e3641d7e1003994239f50547903f
Georg Kromer <georgkromer@protonmail.com>
2020-06-18 14:15:16 +0200

    !!! you need to delete your database once to run this -> added comments and changed name of Database Column

--- src/main/java/com/example/studileih/Controller/UserController.java
+++ src/main/java/com/example/studileih/Controller/UserController.java
@@ -25,36 +25,22 @@ public class UserController {
     @Autowired
     private ProductService ProductService;
 
+    /**
+     * Die Funktion wird direkt nach Start aufgerufen und speichert 1 Beispielwohnheim/Adresse/2 Pro in die DB -> Kann später auskommentiert/gelöscht werden
+     */
     @PostConstruct
     public void createBaseDataset() {
         // can be deleted later
-        Dorm inDerAu = new Dorm("In der Au 16");
-        inDerAu.setCreatedAt(Calendar.getInstance().getTime());
-        inDerAu.setUpdatedAt(Calendar.getInstance().getTime());
+        Dorm inDerAu = new Dorm("In der Au 16");  // Für später: inDerAu.setCreatedAt(Calendar.getInstance().getTime());
         Address inderAuAddress = new Address("In der Au 16", inDerAu);
         Product product1 = new Product("Haralds VW Golf");
-        product1.setCreatedAt(Calendar.getInstance().getTime());
-        product1.setUpdatedAt(Calendar.getInstance().getTime());
-
-        Product product2 = new Product("Haralds 2. VW Golf");
-        product2.setCreatedAt(Calendar.getInstance().getTime());
-        product2.setUpdatedAt(Calendar.getInstance().getTime());
         List<Product> haraldsList = new ArrayList<>();
         haraldsList.add(product1);
-        haraldsList.add(product2);
-
         User user = new User("Harald", "harald@gmx.com", "2345", haraldsList, inDerAu);
-        user.setCreatedAt(Calendar.getInstance().getTime());
-        user.setUpdatedAt(Calendar.getInstance().getTime());
         userService.saveOrUpdateUser(user);
+
         product1.setUser(user);
-        product2.setUser(user);
         ProductService.saveOrUpdateProduct(product1);
-        ProductService.saveOrUpdateProduct(product2);
-       /* Optional<User> user2 = userServiceImpl.getUserById(23L);
-        User user3 = user2.get();
-        user3.setUpdatedAt(Calendar.getInstance().getTime());
-        userServiceImpl.saveOrUpdateUser(user3); */
     }
 
     /**
