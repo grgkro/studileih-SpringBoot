577e7f430e3e4c32dd16c9cbf9a5e7fc82af7106
Georg <georgkromer@pm.me>
2020-07-05 16:37:07 +0200

    deleting product photos works

--- src/main/java/com/example/studileih/Service/ImageService.java
+++ src/main/java/com/example/studileih/Service/ImageService.java
@@ -95,6 +95,40 @@ public class ImageService {
     /*
      * returns a response with the product pic. If the image couldn't be loaded, the response will contain an error message
      */
+    public Resource loadImageByFilenameAsResource(String filename, String type, Long id) {
+        if (type.equals("userPic")) {
+                Path imageFolderLocation = Paths.get(parentFolderLocation + "/users/user" + id);
+                Path filePath = imageFolderLocation.resolve(filename);
+            try {
+                Resource resource = new UrlResource(filePath.toUri());
+                if (resource.exists() || resource.isReadable()) {
+                    return resource;
+                } else {
+                    throw new RuntimeException("It seems like you deleted the images on the server, without also deleting them in the database ");
+                }
+            }catch (MalformedURLException e) {
+                throw new RuntimeException("It seems like you deleted the images on the server, without deleting them in the database ");
+            }
+        } else if (type.equals(("productPic"))) {
+                Path imageFolderLocation = Paths.get(parentFolderLocation + "/products/product" + id); // Each product has an own image folder. imageFolderLocation is the location of that folder.
+                Path filePath = imageFolderLocation.resolve(filename);
+                try {
+                    Resource resource = new UrlResource(filePath.toUri());
+                    if (resource.exists() || resource.isReadable()) {
+                        return resource;
+                    } else {
+                        throw new RuntimeException("It seems like you deleted the images on the server, without also deleting them in the database ");
+                    }
+            } catch (Exception e) {
+                    throw new RuntimeException("It seems like you deleted the images on the server, without deleting them in the database ");
+            }
+        }
+        throw new RuntimeException("Unexpected error when loading the image in backend");
+    }
+
+    /*
+     * returns a response with the product pic. If the image couldn't be loaded, the response will contain an error message
+     */
     public ResponseEntity loadImageByFilename(String filename, Long productId) {
         Path imageFolderLocation = Paths.get(parentFolderLocation + "/products/product" + productId); // Each product has an own image folder. imageFolderLocation is the location of that folder.
         Path filePath = imageFolderLocation.resolve(filename);
@@ -122,14 +156,6 @@ public class ImageService {
 //        FileSystemUtils.deleteRecursively(imageFolderLocation.toFile());
 //    }
 
-    public void deleteImage(File file) {
-        if (file.delete()) // the command in the if statement deletes the file already in the if statement and returns boolean (which we don't use)
-        {
-            System.out.println("Old User profile pic was deleted successfully");
-        } else {
-            System.out.println("Failed to delete the old User profile pic");
-        }
-    }
 
     public void createFolder(Path FolderLocation) {
         if (Files.notExists(FolderLocation)) {
@@ -141,6 +167,18 @@ public class ImageService {
         }
     }
 
+    public ResponseEntity deleteImageByFilename(String filename, String type, long id) {
+        Boolean successfullyDeleted;
+            try {
+                Resource resource = loadImageByFilenameAsResource(filename, type, id);
+                successfullyDeleted = new File(resource.getURI()).delete();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(false);
+            }
+        return ResponseEntity.status(HttpStatus.OK).body(successfullyDeleted);
+
+    }
 }
 
 
