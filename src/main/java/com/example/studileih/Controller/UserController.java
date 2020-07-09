b78af6d6955e21a513a7ba1afab0a3f06b90d8e7
Georg <georgkromer@pm.me>
2020-07-09 11:47:13 +0200

    sending emails works! encrypting works! first JUnit test works!

--- src/main/java/com/example/studileih/Controller/UserController.java
+++ src/main/java/com/example/studileih/Controller/UserController.java
@@ -4,10 +4,13 @@ import com.example.studileih.Dto.UserDto;
 import com.example.studileih.Entity.*;
 import com.example.studileih.Entity.Product;
 import com.example.studileih.Service.DormService;
+import com.example.studileih.Service.EmailService;
 import com.example.studileih.Service.ProductService;
 import com.example.studileih.Service.UserService;
+import org.jasypt.util.text.BasicTextEncryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 
 import javax.annotation.PostConstruct;
 import java.util.*;
@@ -24,6 +27,9 @@ public class UserController {
 
     @Autowired
     private DormService dormService;
+
+    @Autowired
+    private EmailService emailService;
 
     /**
      * Die Funktion wird direkt nach Start aufgerufen und speichert 1 Beispielwohnheim/Adresse/2 Pro in die DB -> Kann später auskommentiert/gelöscht werden
@@ -47,6 +53,10 @@ public class UserController {
         userService.saveOrUpdateUser(harald);
         userService.saveOrUpdateUser(hartmut);
         // durch das Speichern der user werden die verknüpften Produkte auch gespeichert. Es ist also unnötig die Produkte mit productService.saveProduct() nochmal zu speichern.
+
+        // sends an email from studileih@gmail.com. I think you need to be on a Windows PC that this works! else comment it out... (https://www.baeldung.com/spring-email)
+        emailService.sendSimpleMessage("georgkromer@pm.me", "server started", "yolo" );
+
 
 
 
