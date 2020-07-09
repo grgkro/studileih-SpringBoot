5b744f81cc695115909bdeb9a144ee80cba548c8
Georg <georgkromer@pm.me>
2020-07-09 11:47:13 +0200

    sending emails works! encrypting works! first JUnit test works!

--- src/main/java/com/example/studileih/StudileihApplication.java
+++ src/main/java/com/example/studileih/StudileihApplication.java
@@ -1,9 +1,12 @@
 package com.example.studileih;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class StudileihApplication {
 
 	public static void main(String[] args) {
